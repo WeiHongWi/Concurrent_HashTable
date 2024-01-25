e7b50ed3e4df1c34cbc58b28b90976528198c5a7
WeiHongWi <arty963552@gmail.com>
2024-01-25 21:54:34 +0800

    Implementation of hash table with two lists

--- hash_t.h
+++ hash_t.h
@@ -3,15 +3,21 @@
 // The member of the data type are :
 // (1) Pointer of next entry
 // (2) Pointer of element
-// (3) Index number for calculate the hash function in the entry linked list 
-// (4)  
-class hash_t{
-    private:
-        
+// (3) Index number for calculate the hash function in the entry linked list
 
-	
-	public:
+typedef enum {
+  HT_STATUS_CLEAR = 0; HT_STATUS_WRITE; HT_STATUS_READ; HT_STATUS_GROW;
+  HT_STATUS_IDLE;
+  HT_STATUS_READ;
+}ht_status_t;
 
+class hash_t {
+ private:
+  size_t size;
+  size_t max_size;
+  hash_item_list **it;
+  hash_iterator_list *head;
+  ht_status_t status;
 
-
+ public:
 };
--- list.h
+++ list.h
@@ -1,68 +1,60 @@
 #include <iostream>
 
-class ht_item{
-	public:
-    	unsigned int hash;
-		int dlen;
-		ht_item *next;
+class ht_item {
+ public:
+  unsigned int hash;
+  int dlen;
+  ht_item* next;
 
-		ht_item(int hash,int dlen){
-		    this->next = NULL;
-			this->hash = hash;
-			this->dlen = dlen;
-		}
+  ht_item(int hash, int dlen) {
+    this->next = NULL;
+    this->hash = hash;
+    this->dlen = dlen;
+  }
 };
 
-template<class T>
-class list{
-	T* head;
-	public:
-	list(){
-	    this->head = NULL;
-	}
-	list(T* node){
-	    this->head = node;
-	}
+template <class T>
+class list {
+  T* head;
 
-    T* list_head(){
-		return this->head;
-	}
-    
-	bool list_empty(){
-	    return this->head == NULL;
-	}
-    void list_insert(T* n_node);
-	T* list_concat(T& head1 , T& head2);
+ public:
+  list() { this->head = NULL; }
+  list(T* node) { this->head = node; }
 
-	T* list_remove(T* d_node);
-    
-//	list operator=(list l1);
+  T* list_head() { return this->head; }
+
+  bool list_empty() { return this->head == NULL; }
+  void list_insert(T* n_node);
+  T* list_concat(T& head1, T& head2);
+
+  T* list_remove(T* d_node);
+
+  //	list operator=(list l1);
 };
 
-class ht_item_list{
-	private:
-    	size_t index;
-		list<ht_item> head;
-		ht_item_list *next;
-    public:
-		ht_item_list(){
-		    this->head = NULL;
-			this->next = NULL;
-			index = 0;
-		}
+class ht_item_list {
+ private:
+  size_t index;
+  list<ht_item> head;
+  ht_item_list* next;
 
-		list<ht_item>& get_item_list(){
-		    return this->head;
-		}
+ public:
+  ht_item_list() {
+    this->head = NULL;
+    this->next = NULL;
+    index = 0;
+  }
 
-		size_t& get_index(){
-		    return this->index;
-		}
+  list<ht_item>& get_item_list() { return this->head; }
 
- };
-
-class ht_iterator_list{
-    public:
-		list<ht_item_list> head;
+  size_t& get_index() { return this->index; }
 };
 
+class ht_iterator_list {
+ public:
+  list<ht_item_list> head;
+
+  ht_iterator_list() { head = NULL; }
+
+  list<ht_item_list>& get_iterator_list() { return head; }
+};
